41f92dfc786e8f00743cdfcc9e149cd63f6e1002
Seaborn Mercer <surfermercer@gmail.com>
2022-09-16 11:42:23 -0500

    basic database set up

--- scripts/database.js
+++ scripts/database.js
@@ -1,5 +1,15 @@
 const database = {
-    transientState: {}
+    transientState: {},
+    governors: [
+        {id: 1, name: " Glub Shitto", colonyId: 1, active: true}
+    ],
+    colonies: [
+        {id: 1, name: "Europa", governors: [], resources: []}
+    ],
+    facilities: [
+        {id: 1, name: "Io Facility", resources: [], active: true}
+    ],
+    cart: {resources: []}
 }
 
 export const setFacility = (facilityId) => {
@@ -11,10 +21,22 @@ export const getFacilities = () => {
     return database.facilities.map(f => ({...f}))
 }
 
+export const getColonies = () => {
+    return database.colonies.map(c=>({...c}))
+}
+
+export const getGovernors = () => {
+    return database.governors.map(g=>({...g}))
+}
+
+export const getCart = () => {
+    return database.cart.map(c=>({...c}))
+}
+
 export const purchaseMineral = () => {
 
         // Broadcast custom event to entire documement so that the
         // application can re-render and update state
-        document.dispatchEvent( new CustomEvent("stateChanged") )
+        document.dispatchEvent( new CustomEvent("stateChanged") ) {
     }
 }
