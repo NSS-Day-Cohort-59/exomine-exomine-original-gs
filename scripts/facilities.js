b6ef945381a7c8ca0112718e8f2e09d55f20695f
Jacob Thomas <jacgretho@gmail.com>
2022-09-21 10:30:15 -0500

    re-commit

--- scripts/facilities.js
+++ scripts/facilities.js
@@ -5,41 +5,6 @@ const minerals = getMinerals()
 const FacilityResources = getfacilityResources()
 
 
-<<<<<<< HEAD
-        if (event.target.id === "facility") {
-
-            setFacility(parseInt(event.target.value))
-
-            const facilityPrimaryKey = facilityClicked.value
-            let html = "<ul>"
-            let matchedFacility = null
-            for (const facility of facilities) {
-                if (parseInt(facilityPrimaryKey) === facility.id) {
-                    matchedFacility = facility
-                }
-            }
-
-            let matchedMineralFacility = null
-            for (const resources of FacilityResources) {
-                if (matchedFacility.id === resources.facilityId) {
-                    matchedMineralFacility = resources
-                    for (const mineral of minerals) {
-                        if (matchedMineralFacility.mineralId === mineral.id && matchedMineralFacility.quantity > 0) {
-                            html += `<li>
-                    <input type="radio" name="mineral" value="${mineral.id}"/>${matchedMineralFacility.quantity} tons of ${mineral.name}
-                            </li>`
-                        }
-                    }
-                }
-            }
-            html += "</ul>"
-            const parentHTML = document.querySelector(".facility-minerals__display")
-            parentHTML.innerHTML = html;
-        }
-    }
-)
-=======
->>>>>>> main
 
 
 
